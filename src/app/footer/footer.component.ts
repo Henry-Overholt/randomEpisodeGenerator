6dc664b6c16dadb2b954ab48398f349f8feae7d2
Henry Overholt <henrysoverholt@gmail.com>
2020-10-22 17:27:17 -0400

    fixed styling bugs

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
+  links: string[] = [
+    'https://www.themoviedb.org/documentation/api?language=en-US',
+    'http://henryoverholt.surge.sh/',
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  goToLink(i: number): void {
+    window.open(this.links[i], '_blank');
   }
-
 }
