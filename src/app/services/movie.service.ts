1afc187b074656f20546bdd22ad4915b573e6754
Henry Overholt <henrysoverholt@gmail.com>
2020-11-20 14:05:19 -0500

    generated two new services - movie and user

---
+++ src/app/services/movie.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from './../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MovieService {
+  apiKey: string = environment.movieDB.apiKey;
+  constructor(private http: HttpClient) {}
+}
